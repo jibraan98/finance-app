292d7655287ddb9d1a432d6af6b29d657844e4c5
jibraan98 <jibraan2010@hotmail.com>
2024-12-24 12:11:11 +0400

    added account and category pages

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function Home() {
+  const { onOpen } = useNewAccount();
   return (
    <div>
-      finance app
-      <UserButton afterSignOutUrl="/"/>
+      <Button onClick={onOpen}>
+        Add an account
+      </Button>
    </div>
   );
 }
