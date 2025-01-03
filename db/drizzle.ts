dcf8f5311c4cc40fbf841e28ce1f49ee0bdace62
jibraan98 <jibraan2010@hotmail.com>
2025-01-03 18:25:14 +0400

    updated db and connection

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,13 +1,20 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Client } from 'pg';
+// import { drizzle } from 'drizzle-orm/node-postgres';
+// import { Client } from 'pg';
+
+// import * as schema from "./schema";
+
+// const client = new Client({
+//     connectionString: process.env.DATABASE_URL,
+// });
+
+// client.connect();
+
+// export const db = drizzle(client, { schema });
+
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-});
-
-client.connect();
-
-export const db = drizzle(client, { schema });
-
+export const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema })
