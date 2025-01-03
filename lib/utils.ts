73860fdc8c29f55a7ada30be8cc833ab73b7e9bb
jibraan98 <jibraan2010@hotmail.com>
2025-01-03 15:40:45 +0400

    removed comment

--- lib/utils.ts
+++ lib/utils.ts
@@ -70,20 +70,6 @@ type Period = {
   to: string | Date | undefined;
 }
 
-// export function formatDateRange (period?: Period) {
-//   const defaultTo = new Date();
-//   const defaultFrom = subDays(defaultTo, 30);
-
-//   if (!period?.from) {
-//     return `${format(defaultFrom, "LLL dd")} - ${format(defaultTo, "LLL dd, y")}`;
-//   }
-
-//   if (period.to) {
-//     return `${format(period.from, "LLL dd")} - ${format(period.to, "LLL dd, y")}`;
-//   }
-
-//   return format(period.from, "LLL dd, y");
-// }
 export function formatDateRange(period?: Period) {
   const defaultTo = new Date();
   const defaultFrom = subDays(defaultTo, 30);
