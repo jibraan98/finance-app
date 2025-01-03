1ee5b5f85ca270bb31cdc188e369e18e674ddbeb
jibraan98 <jibraan2010@hotmail.com>
2025-01-03 15:39:11 +0400

    added charts and filters

--- features/transactions/api/use-bulk-create-transactions.ts
+++ features/transactions/api/use-bulk-create-transactions.ts
@@ -18,6 +18,7 @@ export const useBulkCreateTransactions = () => {
         onSuccess: () => {
             toast.success("transactions created");
             queryClient.invalidateQueries({ queryKey: ["transactions"]});
+            queryClient.invalidateQueries({ queryKey: ["summary"]});
         },
         onError: () => {
             toast.error("Failed to create transactions");
